a7ffa5b4e6ac64cf69f306e9f2b5996704a8d296
giusber2005 <156716475+giusber2005@users.noreply.github.com>
2024-11-07 22:46:28 +0100

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import { nFormatter } from "@/lib/utils";
 
 export default async function Home() {
   const { stargazers_count: stars } = await fetch(
-    "https://api.github.com/repos/steven-tey/precedent",
+    "https://github.com/giusber2005/project-name",
     {
       ...(process.env.GITHUB_OAUTH_TOKEN && {
         headers: {
